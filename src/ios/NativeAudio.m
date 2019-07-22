3f81095be2e31551d3c9117378e48ce7cffb0b47
n-futagami <n-futagami@cloudpack.jp>
2019-07-22 13:11:32 +0900

    Sound stop with slient mode on iOS

--- src/ios/NativeAudio.m
+++ src/ios/NativeAudio.m
@@ -29,7 +29,6 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
 {
     self.fadeMusic = NO;
 
-    AudioSessionInitialize(NULL, NULL, nil , nil);
     AVAudioSession *session = [AVAudioSession sharedInstance];
     // we activate the audio session after the options to mix with others is set
     [session setActive: NO error: nil];
@@ -37,7 +36,7 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
 
     // Allows the application to mix its audio with audio from other apps.
     if (![session setCategory:AVAudioSessionCategoryAmbient
-                  withOptions:AVAudioSessionCategoryOptionMixWithOthers
+                  withOptions:AVAudioSessionCategoryOptionDuckOthers
                         error:&setCategoryError]) {
 
         NSLog (@"Error setting audio session category.");
@@ -45,7 +44,6 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
     }
 
     [session setActive: YES error: nil];
-    [session setCategory:AVAudioSessionCategoryPlayback error:nil];
 }
 
 - (void) parseOptions:(NSDictionary*) options
